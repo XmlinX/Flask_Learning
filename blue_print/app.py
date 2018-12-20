fafd43e6bd440dac839e5fbd9d90c61eeee4a8c8
json <547816252@qq.com>
2018-12-20 22:01:16 +0800

    蓝图

---
+++ blue_print/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from blueprints.users import user_bp
+
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.register_blueprint(user_bp)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ blue_print/blueprints/users.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+
+user_bp = Blueprint('user', __name__, url_prefix='/user')
+
+@user_bp.route('/profile/')
+def profile():
+    return "这是用户界面"
+
+
+@user_bp.route('/setting/')
+def setting():
+    return "这是用户设置界面"
