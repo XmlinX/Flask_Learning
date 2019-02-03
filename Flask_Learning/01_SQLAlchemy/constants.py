e3202425c7cfb22e55837093f5723b28a3209541
json_xia <547816252@qq.com>
2019-02-03 11:24:30 +0800

    删除

--- Flask_Learning/01_SQLAlchemy/constants.py
+++
@@ -1,8 +0,0 @@
-HOSTNAME = '192.168.31.47'
-PORT = '3306'
-DATABASE = 'demo_test'
-USERNAME = 'root'
-PASSWORD = 'root'
-
-#dialect+driver://username:password@host:port/database
-DB_URI = 'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'.format(username=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT, database=DATABASE)
